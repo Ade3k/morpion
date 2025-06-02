affc367fdcb77fa7125d84fa7bc292a9f4f7d667
Adams <153105032+Ade3k@users.noreply.github.com>
2025-06-02 12:11:15 +0200

    Update environment.ts

--- front/src/environment/environment.ts
+++ front/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://morpion-neon.vercel.app'
   //apiUrl: 'https://morpion-adam-back.vercel.app',
   // D'autres variables que vous pourriez vouloir par défaut
 };
